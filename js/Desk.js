482cbdeaaf7aa5dafc9f64d3282f4519ee9b028e
thegaming-png <70883614+thegaming-png@users.noreply.github.com>
2022-02-19 16:31:09 +0530

    Add files via upload

---
+++ js/Desk.js
@@ -0,0 +1,35 @@
+document.getElementById('backArrow').addEventListener('click', () => {
+    window.location = '../index.html'
+});
+
+
+function preload(){
+    img = loadImage("phone.jfif");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = 'Status: Detecting Objects'
+
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+}
+
+function draw() {
+    // img = 
+
+    image(img, 0, 0, 640, 420);
+    // objectDetector.detect(video, gotResult);
+    
+}
+
+function modelLoaded() {
+    console.log("MODEL LOADED")
+}
+
+// function gotResult() {
+//     console.log('gotResult')
+// }
---
+++ js/app.js
@@ -0,0 +1,19 @@
+document.getElementById('Bed').addEventListener('click', () => {
+    window.location = './pages/bedroom.html';
+});
+
+document.getElementById('tv').addEventListener('click', () => {
+    window.location = './pages/tvAc.html';
+});
+
+document.getElementById('desk').addEventListener('click', () => {
+    window.location = './pages/Desk.html';
+});
+
+document.getElementById('bottles').addEventListener('click', () => {
+    window.location = './pages/Bottles.html';
+});
+
+document.getElementById('basket').addEventListener('click', () => {
+    window.location = './pages/FruitBasket.html';
+});
